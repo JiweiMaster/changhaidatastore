805853c72f510f2f96e89a7d4ac1b772d20cbcdf
Helean-W <helean_w@foxmail.com>
2021-09-13 18:08:07 +0800

    first page

---
+++ src/components/FirstPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="firstPage">
+    <h1>胰腺病例录入系统</h1>
+    <Button class="btn" color="primary" elevation="2" outlined>
+      进入系统
+    </Button>
+    <img src="../assets/foot.png" alt="" />
+  </div>
+</template>
+
+<script>
+  import Button from 'vant/lib/button'
+  export default {
+    name: 'FirstPage',
+    components: {
+      Button
+    }
+  }
+</script>
+
+<style scoped>
+  .firstPage {
+    margin-top: 10vh;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    text-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+    color: rgb(7, 60, 110);
+  }
+  .btn {
+    margin: 10vh auto;
+    font-weight: 800;
+  }
+  img {
+    margin: 40vh auto;
+    width: 90vw;
+    bottom: 0;
+  }
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,63 @@
 <template>
-  <div>
-    <van-tabbar v-model="active">
+  <div class="firstPage">
+    <!-- <van-tabbar v-model="active">
       <van-tabbar-item icon="home-o">标签</van-tabbar-item>
       <van-tabbar-item icon="search">标签</van-tabbar-item>
       <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
       <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
-    </van-tabbar>
+    </van-tabbar> -->
+    <h1>胰腺病例录入系统</h1>
+    <van-button class="btn" plain type="info" text="进入系统"></van-button>
+    <img src="../assets/foot.png" alt="" />
   </div>
 </template>
 
 <script>
-import Button from 'vant/lib/button';
-
-export default {
-  name: 'HelloWorld',
-  components: {
-    Button
-  },
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      active: 0
+  export default {
+    name: 'HelloWorld',
+    components: {},
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        active: 0
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .firstPage {
+    margin-top: 10vh;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    text-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+    color: rgb(7, 60, 110);
+  }
+  .btn {
+    margin: 10vh auto;
+    font-weight: 600;
+    color: black;
+  }
+  img {
+    margin: 40vh auto;
+    width: 90vw;
+    bottom: 0;
+  }
+  /* h3 {
+                        margin: 40px 0 0;
+                      }
+                      ul {
+                        list-style-type: none;
+                        padding: 0;
+                      }
+                      li {
+                        display: inline-block;
+                        margin: 0 10px;
+                      }
+                      a {
+                        color: #42b983;
+                      } */
 </style>
