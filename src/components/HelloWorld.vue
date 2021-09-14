1153acb8e3897ed10c1267f37e2a6301826cbfb4
JiweiMaster <2413914266@qq.com>
2021-09-14 09:16:09 +0800

    edit by jiwie

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,32 @@
 <template>
   <div>
+    <van-nav-bar
+        title="病例数据录入"
+        left-text="返回"
+        left-arrow
+        @click-left="returnLastPage()"
+    />
+    <div style="margin-top: 70vh; margin-left: 60vw">
+      <van-image
+          round
+          width="3rem"
+          height="3rem"
+          :src="require('./icon_edit.png')"
+      />
+    </div>
     <van-tabbar v-model="active">
-      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-      <van-tabbar-item icon="search">标签</van-tabbar-item>
-      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
-      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="add-o">新建</van-tabbar-item>
+      <van-tabbar-item icon="todo-list-o">编辑</van-tabbar-item>
+      <van-tabbar-item icon="passed">已上传</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 
 <script>
-import Button from 'vant/lib/button';
 
 export default {
   name: 'HelloWorld',
   components: {
-    Button
   },
   props: {
     msg: String
@@ -24,12 +35,30 @@ export default {
     return {
       active: 0
     }
+  },
+  methods:{
+    returnLastPage(){
+      console.log('返回到上一个界面...')
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+
+/*.van-nav-bar{*/
+/*  background-color: #913935 !important;*/
+/*}*/
+/*.van-nav-bar .van-icon{*/
+/*  color: #fff !important;*/
+/*}*/
+/*.van-nav-bar__text{*/
+/*  color: #fff !important;*/
+/*}*/
+/*.van-nav-bar__title{*/
+/*  color: #fff !important;*/
+/*}*/
 h3 {
   margin: 40px 0 0;
 }
@@ -44,4 +73,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
